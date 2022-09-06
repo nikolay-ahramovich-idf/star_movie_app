767db48225f5b1ce4074240c3e91ad43954eb3b7
nikolay-ahramovich-idf <admin@Nickolais-MacBook-Air.local>
2022-09-06 11:20:21 +0300

    Fix Navigation logic is added

--- packages/presentation/lib/star_movie_app.dart
+++ packages/presentation/lib/star_movie_app.dart
@@ -1,17 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:presentation/app/app_bloc.dart';
+import 'package:presentation/app/data/app_data.dart';
+import 'package:presentation/bloc/base/bloc_screen.dart';
 import 'package:presentation/screens/splash_screen.dart';
 
-class StarMovieApp extends StatelessWidget {
-  const StarMovieApp({Key? key}) : super(key: key);
+class StarMovieApp extends StatefulWidget {
+  const StarMovieApp({super.key});
 
+  @override
+  State<StarMovieApp> createState() => _StarMovieAppState();
+}
+
+class _StarMovieAppState extends BlocScreenState<StarMovieApp, AppBloc> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Navigator(
-        pages: [
-          SplashScreen.page(),
-        ],
-        onPopPage: (route, result) => route.didPop(result),
+      home: StreamBuilder(
+        stream: bloc.stream,
+        builder: (context, snapshot) {
+          final appData = snapshot.data;
+          if (appData != null) {
+            return Scaffold(
+              body: Navigator(
+                onPopPage: (Route<dynamic> route, dynamic result) {
+                  bloc.handleRemoveRouteSettings(route.settings);
+                  return route.didPop(result);
+                },
+                pages: appData.pages.toList(),
+              ),
+            );
+          }
+          return Container();
+        },
       ),
     );
   }
