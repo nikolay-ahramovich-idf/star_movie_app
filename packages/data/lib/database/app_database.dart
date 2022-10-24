c8f24329232633b600b9177e05f58387ab0d41c1
nikolay-ahramovich-idf <nikolay.ahramovich@idfinance.com>
2022-10-24 13:26:36 +0300

    Fix db entities list formatting

--- packages/data/lib/database/app_database.dart
+++ packages/data/lib/database/app_database.dart
@@ -1,6 +1,4 @@
 import 'dart:async';
-import 'package:floor/floor.dart';
-import 'package:sqflite/sqflite.dart' as sqflite;
 
 import 'package:data/database/dao/genre_dao.dart';
 import 'package:data/database/dao/movie_character_dao.dart';
@@ -8,10 +6,16 @@ import 'package:data/database/dao/movie_dao.dart';
 import 'package:data/database/entities/genre.dart';
 import 'package:data/database/entities/movie.dart';
 import 'package:data/database/entities/movie_character.dart';
+import 'package:floor/floor.dart';
+import 'package:sqflite/sqflite.dart' as sqflite;
 
 part 'app_database.g.dart';
 
-@Database(version: 1, entities: [Movie, Genre, MovieCharacter])
+@Database(version: 1, entities: [
+  Movie,
+  Genre,
+  MovieCharacter,
+])
 abstract class AppDatabase extends FloorDatabase {
   MovieDao get movieDao;
   GenreDao get genreDao;
